9b41a38e2f383cdbce3973b7f9add19dfecf8ca1
ALTIMETRIK\Sveerakumar <sveerakumar@altimetrik.com>
2019-01-23 20:21:27 +0530

    basic UI completed

--- code/src/app/app.component.ts
+++ code/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app';
+
+  ngAfterViewInit(): void {
+
+    // // using context
+    // $('.ui.sidebar')
+    //   .sidebar({
+    //     context: $('.bottom.segment')
+    //   })
+    //   .sidebar('attach events', '.menu .item')
+    //   ;
+
+    $('.grid-stack').gridstack();
+  }
 }
